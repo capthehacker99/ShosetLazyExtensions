ea4a924feee0f7ef800efa0b9988f886660de1ce
capthehacker99 <pawat.capsule@gmail.com>
2024-05-13 05:52:42 +0700

    Added fwo

---
+++ src/en/f-w-o.lua
@@ -0,0 +1,147 @@
+-- {"id":598221818,"ver":"1.0.0","libVer":"1.0.0","author":"","repo":"","dep":[]}
+
+--- Identification number of the extension.
+--- Should be unique. Should be consistent in all references.
+---
+--- Required.
+---
+--- @type int
+local id = 598221818
+
+--- Name of extension to display to the user.
+--- Should match index.
+---
+--- Required.
+---
+--- @type string
+local name = "Fantasy World Online"
+
+--- Base URL of the extension. Used to open web view in Shosetsu.
+---
+--- Required.
+---
+--- @type string
+local baseURL = "https://f-w-o.com/"
+
+--- URL of the logo.
+---
+--- Optional, Default is empty.
+---
+--- @type string
+local imageURL = "https://f-w-o.com/wp-content/uploads/2017/10/logo.5.png"
+
+--- ChapterType provided by the extension.
+---
+--- Optional, Default is STRING. But please do HTML.
+---
+--- @type ChapterType
+local chapterType = ChapterType.HTML
+
+--- Index that pages start with. For example, the first page of search is index 1.
+---
+--- Optional, Default is 1.
+---
+--- @type number
+local startIndex = 1
+
+--- Shrink the website url down. This is for space saving purposes.
+---
+--- Required.
+---
+--- @param url string Full URL to shrink.
+--- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
+--- @return string Shrunk URL.
+local function shrinkURL(url, _)
+    return url:gsub(".-f-w-o.com/", "")
+end
+
+--- Expand a given URL.
+---
+--- Required.
+---
+--- @param url string Shrunk URL to expand.
+--- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
+--- @return string Full URL.
+local function expandURL(url, _)
+    return baseURL .. url
+end
+
+--- Get a chapter passage based on its chapterURL.
+---
+--- Required.
+---
+--- @param chapterURL string The chapters shrunken URL.
+--- @return string Strings in lua are byte arrays. If you are not outputting strings/html you can return a binary stream.
+local function getPassage(chapterURL)
+    local url = expandURL(chapterURL)
+
+    --- Chapter page, extract info from it.
+    local document = GETDocument(url)
+    local htmlElement = document:selectFirst(".entry-content .text-left")
+    return pageOfElem(htmlElement, true)
+end
+
+--- Load info on a novel.
+---
+--- Required.
+---
+--- @param novelURL string shrunken novel url.
+--- @return NovelInfo
+local function parseNovel(novelURL)
+    local url = expandURL(novelURL)
+
+    --- Novel page, extract info from it.
+    local document = GETDocument(url)
+    document:select("script"):remove()
+    local img = document:selectFirst(".summary_image img")
+    img = img and img:attr("src") or imageURL
+    local selected = document:select(".list-chap li a")
+    local cur = selected:size() + 1
+    return NovelInfo({
+        title = document:selectFirst(".post-title h1"):text():gsub("\n" ,""),
+        imageURL = img,
+        chapters = AsList(
+            map(selected, function(v)
+                cur = cur - 1
+                return NovelChapter {
+                    order = cur,
+                    title = v:text(),
+                    link = shrinkURL(v:attr("href"))
+                }
+            end)
+        )
+
+    })
+end
+
+local function getListing()
+    local document = GETDocument(baseURL)
+
+    return map(document:select(".main-col-inner .page-listing-item .item-thumb a"), function(v)
+        return Novel {
+            title = v:attr("title"),
+            link = shrinkURL(v:attr("href")),
+            imageURL = v:selectFirst("img"):attr("arc")
+        }
+    end)
+end
+
+-- Return all properties in a lua table.
+return {
+    -- Required
+    id = id,
+    name = name,
+    baseURL = baseURL,
+    listings = {
+        Listing("Default", false, getListing)
+    }, -- Must have at least one listing
+    getPassage = getPassage,
+    parseNovel = parseNovel,
+    shrinkURL = shrinkURL,
+    expandURL = expandURL,
+    hasSearch = false,
+    -- Optional values to change
+    imageURL = imageURL,
+    chapterType = chapterType,
+    startIndex = startIndex,
+}
