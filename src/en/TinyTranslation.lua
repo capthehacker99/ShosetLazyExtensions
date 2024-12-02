3208b6236d4bda3874e80a33d8f0b9e1297eb8fc
capthehacker99 <pawat.capsule@gmail.com>
2024-12-02 14:51:07 +0700

    Added tiny translation

---
+++ src/en/TinyTranslation.lua
@@ -0,0 +1,160 @@
+-- {"id":1141686301,"ver":"1.0.0","libVer":"1.0.0","author":"","repo":"","dep":[]}
+
+--- Identification number of the extension.
+--- Should be unique. Should be consistent in all references.
+---
+--- Required.
+---
+--- @type int
+local id = 1141686301
+
+--- Name of extension to display to the user.
+--- Should match index.
+---
+--- Required.
+---
+--- @type string
+local name = "Tiny Translation"
+
+--- Base URL of the extension. Used to open web view in Shosetsu.
+---
+--- Required.
+---
+--- @type string
+local baseURL = "https://www.tinytranslation.xyz/"
+
+--- URL of the logo.
+---
+--- Optional, Default is empty.
+---
+--- @type string
+local imageURL = "https://www.tinytranslation.xyz/wp-content/uploads/2023/06/Logo-new.png"
+--- ChapterType provided by the extension.
+---
+--- Optional, Default is STRING. But please do HTML.
+---
+--- @type ChapterType
+local chapterType = ChapterType.HTML
+
+--- Index that pages start with. For example, the first page of search is index 1.
+---
+--- Optional, Default is 1.
+---
+--- @type number
+local startIndex = 1
+
+--- Shrink the website url down. This is for space saving purposes.
+---
+--- Required.
+---
+--- @param url string Full URL to shrink.
+--- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
+--- @return string Shrunk URL.
+local function shrinkURL(url, _)
+    return url:gsub(".-tinytranslation.xyz/", "")
+end
+
+--- Expand a given URL.
+---
+--- Required.
+---
+--- @param url string Shrunk URL to expand.
+--- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
+--- @return string Full URL.
+local function expandURL(url, _)
+	return baseURL .. url
+end
+
+--- Get a chapter passage based on its chapterURL.
+---
+--- Required.
+---
+--- @param chapterURL string The chapters shrunken URL.
+--- @return string Strings in lua are byte arrays. If you are not outputting strings/html you can return a binary stream.
+local function getPassage(chapterURL)
+	local url = expandURL(chapterURL)
+
+	--- Chapter page, extract info from it.
+	local document = GETDocument(url)
+    local htmlElement = document:selectFirst(".content")
+    htmlElement:select("p[style],navigate"):remove()
+    return pageOfElem(htmlElement, true)
+end
+
+--- Load info on a novel.
+---
+--- Required.
+---
+--- @param novelURL string shrunken novel url.
+--- @return NovelInfo
+local function parseNovel(novelURL)
+	local url = expandURL(novelURL)
+	--- Novel page, extract info from it.
+	local document = GETDocument(url)
+    local title = document:selectFirst("title-content")
+    title = title and title:text() or "Unknown Title"
+    return NovelInfo({
+        title = title,
+        imageURL = imageURL,
+        description = desc,
+        chapters = AsList(mapNotNil(document:select(".entry-content ul > li > a"), function(v)
+            local link = v:attr("href")
+            if not link:find("tinytranslation") then return end
+            return NovelChapter {
+                order = v,
+                title = v:text(),
+                link = shrinkURL(link)
+            }
+        end))
+    })
+end
+
+local function getListing()
+    local document = GETDocument(expandURL("list-novels/"))
+
+    return mapNotNil(document:select("ul.series-posts-list > li > a"), function(v)
+        local link = v:attr("href")
+        if not link:find("series") then return end
+        return Novel {
+            title = v:text(),
+            link = shrinkURL(link),
+            imageURL = imageURL
+        }
+    end)
+end
+
+local function search(data)
+    local page = data[PAGE]
+    local query = data[QUERY]
+    local document = GETDocument(expandURL("?s=" .. query .. "&paged=" .. page .. "/"))
+    return mapNotNil(document:select(".search-results > div.latest-postItemContainer > a"), function(v)
+        local link = v:attr("href")
+        if not link:find("series") then return end
+        return Novel {
+            title = v:text(),
+            link = shrinkURL(link),
+            imageURL = imageURL
+        }
+    end)
+end
+
+-- Return all properties in a lua table.
+return {
+	-- Required
+	id = id,
+	name = name,
+	baseURL = baseURL,
+	listings = {
+        Listing("Default", false, getListing)
+    }, -- Must have at least one listing
+	getPassage = getPassage,
+	parseNovel = parseNovel,
+	shrinkURL = shrinkURL,
+	expandURL = expandURL,
+    hasSearch = true,
+    isSearchIncrementing = true,
+    search = search,
+	imageURL = imageURL,
+	chapterType = chapterType,
+	startIndex = startIndex,
+}
