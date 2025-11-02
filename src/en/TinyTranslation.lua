7008fdd950e94d87496aaac34a1e362b7e379293
capthehacker99 <pawat.capsule@gmail.com>
2025-11-02 11:05:49 +0700

    fix: remove home disclamer, contact, etc from TinyTranslations

--- src/en/TinyTranslation.lua
+++ src/en/TinyTranslation.lua
@@ -1,4 +1,4 @@
--- {"id":1141686301,"ver":"1.0.7","libVer":"1.0.6","author":"","repo":"","dep":[]}
+-- {"id":1141686301,"ver":"1.0.8","libVer":"1.0.6","author":"","repo":"","dep":[]}
 
 --- Identification number of the extension.
 --- Should be unique. Should be consistent in all references.
@@ -104,6 +104,10 @@ local function parseNovel(novelURL)
         chapters = AsList(mapNotNil(document:select("#info ul > li a, .entry-content div.container ul > li a, .entry-content ul > li a"), function(v)
             local link = v:attr("href")
             if not (link:find("tinytranslation") or link:find("^/")) then return end
+            if link == "/disclaimer/" or link == "/terms-of-services/" or link == "/donations/" or link == "/contact-us/" or link == "/list-novels/" or link == "https://www.tinytranslation.xyz/privacy-policy/" or link == "https://www.tinytranslation.xyz" then
+                return
+            end
+            print(link)
             return NovelChapter {
                 order = v,
                 title = v:text(),
