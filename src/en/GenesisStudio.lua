de437115e4f9e8928bb1ece0e6e7dee077733333
capthehacker99 <pawat.capsule@gmail.com>
2026-07-14 21:48:10 +0700

    fix: genesis studio and otaku translation
    Closes: #6

--- src/en/GenesisStudio.lua
+++ src/en/GenesisStudio.lua
@@ -1,465 +1,106 @@
--- {"id":1035923222,"ver":"1.0.10","libVer":"1.0.10","author":"","repo":"","dep":[]}
+-- {"id":1035923222,"ver":"1.1.0","libVer":"1.0.0","author":"","repo":"","dep":[]}
 local json = Require("dkjson")
---- Identification number of the extension.
---- Should be unique. Should be consistent in all references.
----
---- Required.
----
---- @type int
+
 local id = 1035923222
-
---- Name of extension to display to the user.
---- Should match index.
----
---- Required.
----
---- @type string
 local name = "Genesis Studio"
-
---- Base URL of the extension. Used to open web view in Shosetsu.
----
---- Required.
----
---- @type string
 local baseURL = "https://genesistudio.com/"
-
---- URL of the logo.
----
---- Optional, Default is empty.
----
---- @type string
 local imageURL = "https://genesistudio.com/favicon-32x32.png"
---- ChapterType provided by the extension.
----
---- Optional, Default is STRING. But please do HTML.
----
---- @type ChapterType
 local chapterType = ChapterType.HTML
-
---- Index that pages start with. For example, the first page of search is index 1.
----
---- Optional, Default is 1.
----
---- @type number
 local startIndex = 1
 
---- Shrink the website url down. This is for space saving purposes.
----
---- Required.
----
---- @param url string Full URL to shrink.
---- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
---- @return string Shrunk URL.
 local function shrinkURL(url, _)
     return url:gsub(".-genesistudio.com/", "")
 end
 
---- Expand a given URL.
----
---- Required.
----
---- @param url string Shrunk URL to expand.
---- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
---- @return string Full URL.
 local function expandURL(url, _)
-	return baseURL .. url
+    return baseURL .. url
 end
 
-
--- Library shit
-local function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k,v in pairs(o) do
-            if type(k) ~= 'number' then k = '"'..k..'"' end
-            s = s .. '['..k..'] = ' .. dump(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
+local function getCoverURL(novel)
+    if novel.coverFile and novel.coverFile.filename_disk then
+        return "https://genesistudio.com/cdn-cgi/image/width=256,format=avif/https://api.genesistudio.com/storage/v1/object/public/directus/" .. novel.coverFile.filename_disk
     end
+    return imageURL
 end
 
-local skip_ws;
-local parse_expr;
-local parse_table;
-local parse_array;
-local parse_key;
-local parse_string;
-skip_ws = function(lex)
-    local chr;
-    while true do
-        chr = lex[1]:sub(lex[2], lex[2])
-        if chr ~= " " and chr ~= "\n" and chr ~= "\t" and chr ~= "\r" then
-            return
-        end
-        lex[2] = lex[2] + 1
-    end
-end
-
-
-parse_string = function(lex)
-    local chr = lex[1]:sub(lex[2], lex[2])
-    if chr ~= '"' and chr ~= "'" then
-        error("[STR] Expected `\"` or `'` got " .. lex[1]:sub(lex[2]))
-    end
-    local og = chr
-    local es = false
-    lex[2] = lex[2] + 1
-    local beg = lex[2]
-    local total_str = ""
-    while true do
-        chr = lex[1]:sub(lex[2], lex[2])
-        if chr == "" then
-            error("[STR] Unterminated string.")
-        end
-        if not es and chr == og then
-            lex[2] = lex[2] + 1
-            return total_str
-        end
-        if es then
-            if chr == '\\' then
-                total_str = total_str .. chr
-            elseif chr == 'b' then
-                total_str = total_str .. '\b'
-            elseif chr == 'f' then
-                total_str = total_str .. '\f'
-            elseif chr == 'n' then
-                total_str = total_str .. '\n'
-            elseif chr == 'r' then
-                total_str = total_str .. '\r'
-            elseif chr == 't' then
-                total_str = total_str .. '\t'
-            elseif chr == 'v' then
-                total_str = total_str .. '\v'
-            elseif chr == '\'' then
-                total_str = total_str .. '\''
-            elseif chr == '\"' then
-                total_str = total_str .. '\"'
-            else
-                local match = lex[1]:sub(lex[2]):match("^([0-7]?[0-7]?[0-7])")
-                if match then
-                    total_str = total_str .. string.char(tonumber(match, 8))
-                    lex[2] = lex[2] + #match
-                else
-                    match = lex[1]:sub(lex[2]):match("^x(%x%x)")
-                    if match then
-                        total_str = total_str .. string.char(tonumber(match, 16))
-                        lex[2] = lex[2] + #match
-                    else
-                        match = lex[1]:sub(lex[2]):match("^u(%x%x%x%x)")
-                        if match then
-                            lex[2] = lex[2] + #match
-                        end
-                    end
-                end
-            end
-            es = false
-        elseif chr == '\\' then
-            es = true
-        else
-            total_str = total_str .. chr
-        end
-        lex[2] = lex[2] + 1
-    end
-end
-
-parse_key = function(lex)
-    local chr = lex[1]:sub(lex[2], lex[2])
-    if chr == '"' or chr == "'" then
-        return parse_string(lex)
-    end
-    local l, r = lex[1]:sub(lex[2]):find("^[_$%l%u]+[_$%w]*")
-    if not l then
-        error("[KEY] Expected identifier got " .. lex[1]:sub(lex[2]))
-    end
-    local id = lex[1]:sub(lex[2] + l - 1, lex[2] + r - 1)
-    lex[2] = lex[2] + r
-    return id
-end
-
-parse_table = function(lex)
-    local chr = lex[1]:sub(lex[2], lex[2])
-    if chr ~= "{" then
-        error("[TAB] Expected { at " .. lex[1]:sub(lex[2]))
-    end
-    lex[2] = lex[2] + 1
-    skip_ws(lex)
-    if lex[1]:sub(lex[2], lex[2]) == "}" then
-        lex[2] = lex[2] + 1
-        return {}
-    end
-    local arr = {}
-    while true do
-        skip_ws(lex)
-        local key = parse_key(lex)
-        -- print("Parsed key " .. dump(key))
-        skip_ws(lex)
-        chr = lex[1]:sub(lex[2], lex[2])
-        if chr ~= ':' then
-            error("[TAB] Expected : at " .. lex[1]:sub(lex[2]))
-        end
-        lex[2] = lex[2] + 1
-        skip_ws(lex)
-        arr[key] = parse_expr(lex)
-        skip_ws(lex)
-        chr = lex[1]:sub(lex[2], lex[2])
-        if chr ~= ',' then
-            break
-        end
-        lex[2] = lex[2] + 1
-    end
-    if chr ~= '}' then
-        error("[TAB] Expected } at " .. lex[1]:sub(lex[2]))
-    end
-    lex[2] = lex[2] + 1
-    -- print("Parsed table " .. dump(arr))
-    return arr
-end
-
-parse_array = function(lex)
-    local chr = lex[1]:sub(lex[2], lex[2])
-    if chr ~= "[" then
-        error("[ARR] Expected [ at " .. lex[1]:sub(lex[2]))
-    end
-    lex[2] = lex[2] + 1
-    skip_ws(lex)
-    if lex[1]:sub(lex[2], lex[2]) == "]" then
-        lex[2] = lex[2] + 1
-        return {}
-    end
-    local arr = {}
-    while true do
-        skip_ws(lex)
-        table.insert(arr, parse_expr(lex))
-        skip_ws(lex)
-        chr = lex[1]:sub(lex[2], lex[2])
-        if chr ~= ',' then
-            break
-        end
-        lex[2] = lex[2] + 1
-    end
-    if chr ~= ']' then
-        error("[ARR] Expected ] at " .. lex[1]:sub(lex[2]))
-    end
-    lex[2] = lex[2] + 1
-    -- print("Parsed arr " .. dump(arr))
-    return arr
-end
-
-parse_expr = function(lex)
-    skip_ws(lex)
-    local chr = lex[1]:sub(lex[2], lex[2])
-    if chr == "[" then
-        return parse_array(lex)
-    elseif chr == "{" then
-        return parse_table(lex)
-    elseif chr == "\"" or chr == "\'" then
-        return parse_string(lex)
-    end
-    if lex[1]:sub(lex[2], lex[2] + 4) == "false" then
-        lex[2] = lex[2] + 5
-        return false
-    end
-    if lex[1]:sub(lex[2], lex[2] + 3) == "true" then
-        lex[2] = lex[2] + 4
-        return true
-    end
-    if lex[1]:sub(lex[2], lex[2] + 3) == "null" then
-        lex[2] = lex[2] + 4
-        return nil
-    end
-    do
-        local l, r = lex[1]:sub(lex[2]):find("^%d+.%d+")
-        if not l then
-            l, r = lex[1]:sub(lex[2]):find("^.%d+")
-        end
-        if not l then
-            l, r = lex[1]:sub(lex[2]):find("^%d+")
-        end
-        if l then
-            local num = lex[1]:sub(lex[2] + l - 1, lex[2] + r - 1)
-            lex[2] = lex[2] + r
-            return tonumber(num)
-        end
-    end
-    error("[EXPR] Unexpected `" .. chr .. "` at " .. lex[1]:sub(lex[2]))
-end
-
-local function parse_obj(tar)
-    local lex = { tar, 1 }
-    return parse_expr(lex)
-end
-
-local function conv2lua(js)
-    js = [[
-local parseInt = function(str)
-    local res = tonumber(str:match("^%d+"))
-    if res == nil then
-        return 9e9999-9e9999
-    end
-    return res
-end
-    ]] .. js
-    local add_back = ""
-    js = js:gsub("===", "==")
-    js = js:gsub("if%s*(%b())([^;]+);%s*else%s*([^;]+);", function(args, l, r)
-        return "if" .. args .. "then " .. l .. " else " .. r .. " end "
-    end)
-    js = js:gsub("return%s+([_$%l%u]+[_$%w]*)%s*=%s*function%s*(%b())%s*(%b{})%s*,", function(name, args, content)
-        content = content:sub(2, #content - 1)
-        return name .. " = " .. "(" .. "function" .. args .. content .. "end)\nreturn "
-    end)
-    js = js:gsub("function%s+([_$%l%u]+[_$%w]*)(%b())%s*(%b{})", function(name, args, content)
-        content = content:sub(2, #content - 1)
-        return name .. " = " .. "function" .. args .. content .. "end "
-    end)
-    js = js:gsub("function%s*(%b())%s*(%b{})", function(args, content)
-        content = content:sub(2, #content - 1)
-        return "(" .. "function" .. args .. content .. "end)"
-    end)
-    js = js:gsub("try%s*(%b{})%s*catch%s*(%b())%s*(%b{})", function(content, excep_var, excep_clause)
-        content = content:sub(2, #content - 1)
-        excep_var = excep_var:sub(2, #excep_var - 1)
-        excep_clause = excep_clause:sub(2, #excep_clause - 1)
-        return "do local __should_break = false local __try_success, " .. excep_var .. " = pcall(function()" .. content:gsub("break","__should_break=true return") .. " end) if __should_break then break end if not __try_success then " .. excep_clause .. " end end "
-    end)
-    js = js:gsub("while%s*(%b())%s*(%b{})", function(args, content)
-        content = content:sub(2, #content - 1)
-        return "while " .. args .. " do " .. content .. " end "
-    end)
-    js = js:gsub("var%s+([_$%l%u]+[_$%w]*)%s*=%s*([^,]+),%s*([_$%l%u]+[_$%w]*)%s*=%s*([^;]+);", function(name, l, name2, r)
-        return "var " .. name .. " = " .. l .. ";var " .. name2 .. " = " .. r .. ";"
-    end)
-    js = js:gsub("var%s+([_$%l%u]+[_$%w]*)", function(name)
-        return "local " .. name
-    end)
-    js = js:gsub("!%[%]", "false")
-    js = js:gsub("!false", "true")
-    js = js:gsub("(%b\'\')%s*:", function(name)
-        return "[" .. name .. "] ="
-    end)
-    js = js:gsub("(%b\"\")%s*:", function(name)
-        return "[" .. name .. "] ="
-    end)
-    js = js:gsub("(%b[])", function(arr)
-        local content = arr:sub(2, #arr - 1)
-        if content:find("^[_$%l%u]+[_$%w]*$") then
-            return "[" .. content .. "+1]"
-        end
-        if content:find(",") then
-            return "{" .. content .. "}"
-        end
-        return arr
-    end)
-    js = js:gsub("([_$%l%u]+[_$%w]*)%['push'%](%b())", function(name, args)
-        return " table.insert(" .. name .. ", " .. args:sub(2, #args - 1) .. ")"
-    end)
-    js = js:gsub("([_$%l%u]+[_$%w]*)%['shift'%](%b())", function(name, args)
-        return " table.remove(" .. name .. ", 1)"
-    end)
-    js = js:gsub("%((%b()%b())%s*,%s*%((%b()%b())%)%)", function(l, r)
-        add_back = l .. ";return " .. r .. ""
-        return ""
-    end)
-    js = js .. add_back
-    return js
-end
-
---- Get a chapter passage based on its chapterURL.
----
---- Required.
----
---- @param chapterURL string The chapters shrunken URL.
---- @return string Strings in lua are byte arrays. If you are not outputting strings/html you can return a binary stream.
 local function getPassage(chapterURL)
-	local url = expandURL(chapterURL)
-
-	--- Chapter page, extract info from it.
-	local document = GETDocument(url)
-    return pageOfElem(document:selectFirst(".novel-content"), true)
-end
-
---- Load info on a novel.
----
---- Required.
----
---- @param novelURL string shrunken novel url.
---- @return NovelInfo
-local function parseNovel(novelURL)
-	local url = expandURL(novelURL)
-	local document = GETDocument(url)
-    local data;
-    map(document:select("script"),function(script)
-        if data then
-            return
-        end
-        local matched = string.match(tostring(script), "{type:\"data\",data:{novel:.+")
-        if not matched then
-            return
-        end
-        matched = string.match(matched, "%b{}")
-        data = parse_obj(matched)
-    end)
-    if not data or not data.data then
-        error("Failed to obtain novel data.")
+    local url = expandURL(chapterURL)
+    local document = GETDocument(url)
+    local htmlElement = document:selectFirst(".novel-content")
+    if htmlElement then
+        return pageOfElem(htmlElement, true)
     end
-    local chapters = {}
-    local prob_list = nil
-    for k, v in next, data.data do
-        if k:match("list") or k:match("chapters") then
-            prob_list = v
+    local slug, chapterNum = chapterURL:match("novels/([^/]+)/chapter%-(%d+)")
+    if not slug then
+        error("Failed to parse chapter URL")
+    end
+    local novelData = json.GET("https://genesistudio.com/api/directus/novels?filter[slug]=" .. slug .. "&limit=1")
+    if not novelData or #novelData == 0 then
+        error("Novel not found: " .. slug)
+    end
+    local novelUUID = novelData[1].id
+    local chaptersResp = json.GET("https://genesistudio.com/api/novels-chapter/" .. novelUUID)
+    local chapters = chaptersResp and chaptersResp.data and chaptersResp.data.chapters or {}
+    local chapterId = nil
+    for _, ch in ipairs(chapters) do
+        if ch.chapter_number == tonumber(chapterNum) then
+            chapterId = ch.id
             break
         end
     end
-    if prob_list == nil then
-        error("Failed to find chapter list, contact @99tracheae")
+    if not chapterId then
+        error("Chapter not found")
     end
-    local chapters_data
-    if type(prob_list) == "table" then
-        chapters_data = prob_list
-    else
-        local lua_script = conv2lua(prob_list)
-        local f, err = load(lua_script)
-        if err then
-            error(err)
-        end
-        chapters_data = f()
+    local contentResp = json.GET("https://genesistudio.com/api/chapters/" .. chapterId .. "/content")
+    if contentResp and contentResp.data and contentResp.data.chapter_content then
+        return contentResp.data.chapter_content
     end
-    local chapters_map = {{}, {}}
-    for _, v1 in next, chapters_data do
-        for _, v2 in next, v1 do
-            if v2.required_tier == 0 and v2.id ~= nil and v2.chapter_number ~= nil then
-                if not chapters_map[1][v2.chapter_number] or not chapters_map[2][v2.id] then
-                    table.insert(chapters, NovelChapter {
-                        order = v2.chapter_number,
-                        title = v2.chapter_title,
-                        link = "viewer/" .. v2.id
-                    })
-                    chapters_map[1][v2.chapter_number] = true
-                    chapters_map[2][v2.id] = true
-                end
-            end
-        end
+    error("Failed to get chapter content")
+end
+
+local function parseNovel(novelURL)
+    local slug = novelURL:match("novels/([^/]+)")
+    if not slug then
+        error("Failed to parse novel URL")
     end
-	return NovelInfo({
-        title = data.data.novel.novel_title,
-        imageURL = data.data.novel.cover,
-        description = data.data.novel.synopsis,
+    local novelData = json.GET("https://genesistudio.com/api/directus/novels?filter[slug]=" .. slug .. "&limit=1")
+    if not novelData or #novelData == 0 then
+        error("Novel not found: " .. slug)
+    end
+    local novel = novelData[1]
+    local chapters = {}
+    local chaptersResp = json.GET("https://genesistudio.com/api/novels-chapter/" .. novel.id)
+    local chaptersData = chaptersResp and chaptersResp.data and chaptersResp.data.chapters or {}
+    for _, ch in ipairs(chaptersData) do
+        table.insert(chapters, NovelChapter {
+            order = ch.chapter_number,
+            title = ch.chapter_title,
+            link = "novels/" .. slug .. "/chapter-" .. ch.chapter_number
+        })
+    end
+    return NovelInfo({
+        title = novel.novel_title,
+        imageURL = getCoverURL(novel),
+        description = novel.synopsis,
         chapters = chapters
     })
 end
 
 local function getListing()
-    local novels_json = json.GET("https://genesistudio.com/api/novels/search?serialization=All&sort=Popular")
+    local data = json.GET("https://genesistudio.com/api/directus/novels?limit=-1")
     local novels = {}
-    for _, novel in pairs(novels_json) do
-        table.insert(novels, Novel {
-            title = novel.novel_title,
-            link = "novels/" .. novel.abbreviation,
-            genres = novel.genres,
-            imageURL = novel.cover and novel.cover or imageURL
-        })
+    if not data then
+        return novels
+    end
+    for _, novel in ipairs(data) do
+        local slug = novel.slug or novel.abbreviation
+        if slug then
+            table.insert(novels, Novel {
+                title = novel.novel_title,
+                link = "novels/" .. slug,
+                imageURL = getCoverURL(novel)
+            })
+        end
     end
     return novels
 end
@@ -477,36 +118,39 @@ end
 
 local function search(data)
     local query = data[QUERY]
-    local novels_json = json.GET("https://genesistudio.com/api/novels/search?serialization=All&sort=Popular&title=" .. urlEncode(query))
+    local result = json.GET("https://genesistudio.com/api/directus/novels?filter[novel_title][_contains]=" .. urlEncode(query) .. "&limit=-1")
     local novels = {}
-    for _, novel in pairs(novels_json) do
-        table.insert(novels, Novel {
-            title = novel.novel_title,
-            link = "novels/" .. novel.abbreviation,
-            genres = novel.genres,
-            imageURL = novel.cover and novel.cover or imageURL
-        })
+    if not result then
+        return novels
+    end
+    for _, novel in ipairs(result) do
+        local slug = novel.slug or novel.abbreviation
+        if slug then
+            table.insert(novels, Novel {
+                title = novel.novel_title,
+                link = "novels/" .. slug,
+                imageURL = getCoverURL(novel)
+            })
+        end
     end
     return novels
 end
 
--- Return all properties in a lua table.
 return {
-	-- Required
-	id = id,
-	name = name,
-	baseURL = baseURL,
-	listings = {
+    id = id,
+    name = name,
+    baseURL = baseURL,
+    listings = {
         Listing("Default", false, getListing)
-    }, -- Must have at least one listing
-	getPassage = getPassage,
-	parseNovel = parseNovel,
-	shrinkURL = shrinkURL,
-	expandURL = expandURL,
+    },
+    getPassage = getPassage,
+    parseNovel = parseNovel,
+    shrinkURL = shrinkURL,
+    expandURL = expandURL,
     hasSearch = true,
     isSearchIncrementing = false,
     search = search,
-	imageURL = imageURL,
-	chapterType = chapterType,
-	startIndex = startIndex,
+    imageURL = imageURL,
+    chapterType = chapterType,
+    startIndex = startIndex,
 }
--- src/en/OtakuTranslation.lua
+++ src/en/OtakuTranslation.lua
@@ -1,167 +1,128 @@
--- {"id":930283970,"ver":"1.0.0","libVer":"1.0.0","author":"","repo":"","dep":[]}
+-- {"id":930283970,"ver":"1.1.0","libVer":"1.0.0","author":"","repo":"","dep":[]}
+local json = Require("dkjson")
 
---- Identification number of the extension.
---- Should be unique. Should be consistent in all references.
----
---- Required.
----
---- @type int
 local id = 930283970
-
---- Name of extension to display to the user.
---- Should match index.
----
---- Required.
----
---- @type string
 local name = "Otaku Translation"
-
---- Base URL of the extension. Used to open web view in Shosetsu.
----
---- Required.
----
---- @type string
 local baseURL = "https://otakutl.blogspot.com/"
-
---- URL of the logo.
----
---- Optional, Default is empty.
----
---- @type string
 local imageURL = "https://otakutl.blogspot.com/favicon.ico"
---- ChapterType provided by the extension.
----
---- Optional, Default is STRING. But please do HTML.
----
---- @type ChapterType
 local chapterType = ChapterType.HTML
-
---- Index that pages start with. For example, the first page of search is index 1.
----
---- Optional, Default is 1.
----
---- @type number
 local startIndex = 1
 
---- Shrink the website url down. This is for space saving purposes.
----
---- Required.
----
---- @param url string Full URL to shrink.
---- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
---- @return string Shrunk URL.
+local genreLabels = {
+    "Romance", "BL", "SCI-FI", "Action", "Adventure", "Comedy", "Fantasy",
+    "Slice of Life", "Eastern fantasy", "Western Fantasy", "Sci-Fi",
+    "Eastern Fantasy", "Western fantasy", "Mystery", "Horror", "Tragedy",
+    "Harem", "Martial Arts", "Psychological", "Supernatural", "Xianxia",
+    "Xuanhuan", "Mature", "Seinen", "Shounen", "Josei", "Yaoi", "Yuri",
+    "Gender Bender", "School Life", "Smut"
+}
+
+local genreSet = {}
+for _, label in ipairs(genreLabels) do
+    genreSet[label] = true
+end
+
+local feedURL = "https://otakutl.blogspot.com/feeds/posts/default?alt=json"
+
 local function shrinkURL(url, _)
     return url
 end
 
---- Expand a given URL.
----
---- Required.
----
---- @param url string Shrunk URL to expand.
---- @param _ int Either KEY_CHAPTER_URL or KEY_NOVEL_URL.
---- @return string Full URL.
 local function expandURL(url, _)
-	return url
+    return url
 end
 
---- Get a chapter passage based on its chapterURL.
----
---- Required.
----
---- @param chapterURL string The chapters shrunken URL.
---- @return string Strings in lua are byte arrays. If you are not outputting strings/html you can return a binary stream.
-local function getPassage(chapterURL)
-	local url = expandURL(chapterURL)
+local function urlEncode(str)
+    if str then
+        str = str:gsub("\n", "\r\n")
+        str = str:gsub("([^%w %-%_%.%~])", function(c)
+            return ("%%%02X"):format(string.byte(c))
+        end)
+        str = str:gsub(" ", "+")
+    end
+    return str
+end
 
-	--- Chapter page, extract info from it.
-	local document = GETDocument(url)
-    local htmlElement = document:selectFirst(".post")
+local function getPassage(chapterURL)
+    local document = GETDocument(chapterURL)
+    local htmlElement = document:selectFirst(".post-body.entry-content")
+    if not htmlElement then
+        htmlElement = document:selectFirst(".post-body")
+    end
     return pageOfElem(htmlElement, true)
 end
 
---- Load info on a novel.
----
---- Required.
----
---- @param novelURL string shrunken novel url.
---- @return NovelInfo
 local function parseNovel(novelURL)
-	local url = expandURL(novelURL)
-
-	--- Novel page, extract info from it.
-	local document = GETDocument(url)
-    local title = document:selectFirst("h3[itemprop=\"name\"]"):text()
-    local img = document:selectFirst("img")
-    img = img and img:attr("src") or "https://picsum.photos/200/300"
-    local desc = ""
-    map(document:select("#editdescription p"), function(p)
-        desc = desc .. '\n' .. p:text()
+    local novelName = novelURL:match("search/label/(.+)$")
+    if not novelName then
+        error("Failed to parse novel name from URL")
+    end
+    novelName = novelName:gsub("+", " "):gsub("%%(%x%x)", function(hex)
+        return string.char(tonumber(hex, 16))
     end)
-	return NovelInfo({
-        title = title,
-        imageURL = img,
-        description = desc,
-        chapters = AsList(
-            mapNotNil(document:select(".entry-content span > a"), function(v)
-                local link = v:attr("href")
-                if not link or link == "" then return end
-                return NovelChapter {
-                    title = v:text(),
-                    link = shrinkURL(link)
-                }
-            end)
-        )
+    local feedData = json.GET(feedURL .. "&category=" .. urlEncode(novelName) .. "&max-results=100")
+    local feed = feedData and feedData.feed
+    if not feed or not feed.entry then
+        error("No entries found for: " .. novelName)
+    end
+    local chapters = {}
+    for i, entry in ipairs(feed.entry) do
+        local link = ""
+        if entry.link and #entry.link > 0 then
+            for _, l in ipairs(entry.link) do
+                if l.rel == "alternate" then
+                    link = l.href
+                    break
+                end
+            end
+        end
+        local chapterTitle = entry.title and entry.title["$t"] or ("Chapter " .. i)
+        table.insert(chapters, NovelChapter {
+            order = i,
+            title = chapterTitle,
+            link = link
+        })
+    end
+    return NovelInfo({
+        title = novelName,
+        imageURL = imageURL,
+        chapters = chapters
     })
 end
 
 local function getListing()
-    local visited = {}
+    local feedData = json.GET(feedURL .. "&max-results=0")
+    local seen = {}
     local novels = {}
-    local function visit(url)
-        if url == "" or visited[url] or not (url:find("otakutl.blogspot.com") or url:find("darbloodage.blogspot.com")) or url:find("privacy-policy.html$") then return end
-        local document = GETDocument(url)
-        visited[url] = true
-        map(document:select(".post"), function(v)
-            local atag = v:selectFirst(".post-title a")
-            if not atag then return end
-            local link = atag:attr("href")
-            if link == "" then return end
-            visited[link] = true
-            local img = v:selectFirst("[itemprop=\"image_url\"]")
-            img = img and img:attr("content") or "https://picsum.photos/200/300"
-            table.insert(
-                novels,
-                Novel {
-                    title = atag:text(),
-                    link = link,
-                    imageURL = img
-                }
-            )
-        end)
-        map(document:select(".entry-content a"), function(v)
-            visit(v:attr("href"))
-        end)
+    if feedData and feedData.feed and feedData.feed.category then
+        for _, cat in ipairs(feedData.feed.category) do
+            local term = cat.term
+            if term and not genreSet[term] and not seen[term] then
+                seen[term] = true
+                table.insert(novels, Novel {
+                    title = term,
+                    link = baseURL .. "search/label/" .. urlEncode(term),
+                    imageURL = imageURL
+                })
+            end
+        end
     end
-    visit("https://otakutl.blogspot.com/p/list-of-translations.html")
     return novels
 end
 
--- Return all properties in a lua table.
 return {
-	-- Required
-	id = id,
-	name = name,
-	baseURL = baseURL,
-	listings = {
+    id = id,
+    name = name,
+    baseURL = baseURL,
+    listings = {
         Listing("Default", false, getListing)
-    }, -- Must have at least one listing
-	getPassage = getPassage,
-	parseNovel = parseNovel,
-	shrinkURL = shrinkURL,
-	expandURL = expandURL,
+    },
+    getPassage = getPassage,
+    parseNovel = parseNovel,
+    shrinkURL = shrinkURL,
+    expandURL = expandURL,
     hasSearch = false,
-	imageURL = imageURL,
-	chapterType = chapterType,
-	startIndex = startIndex,
+    imageURL = imageURL,
+    chapterType = chapterType,
+    startIndex = startIndex,
 }
